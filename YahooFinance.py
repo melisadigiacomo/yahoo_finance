19b7f3e613a1ad95a0213586c0f078a3c28040c4
Melisa <melisa.digiacomo@gmail.com>
2021-07-27 12:31:11 -0300

    update text size in .py and .png

--- YahooFinance.py
+++ YahooFinance.py
@@ -30,12 +30,12 @@ df.to_csv('./YahooFinances_S&P500_5AÑOS.csv')
 
 # Plot Price data
 plt.style.use('ggplot')
-ax = df.plot(x = "Date", y = 'Close',title ="S&P500 Index", fontsize=12, legend = False)
-ax.set_xlabel("Date",fontsize=12)
-ax.set_ylabel("Price",fontsize=12)
-ax.annotate('COVID-19', xy=(914, 3450), xytext=(940, 3600),
+ax = df.plot(x = "Date", y = 'Close',title ="S&P500 Index", fontsize=15, legend = False)
+ax.set_xlabel("Date",fontsize=15)
+ax.set_ylabel("Price",fontsize=15)
+ax.annotate('COVID-19', xy=(914, 3450), xytext=(940, 3600),fontsize=15,
             arrowprops=dict(facecolor='black', shrink=0.05))
-ax.annotate("America's trade war with China", xy=(600, 2796), xytext=(630, 3100),
+ax.annotate("America's trade war with China", xy=(600, 2796), xytext=(630, 3100), fontsize=15,
             arrowprops=dict(facecolor='black', shrink=0.05))
 plt.show()
 
